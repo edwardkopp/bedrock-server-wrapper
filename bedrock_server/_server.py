87edea9fc3c9d44b2e0871160f7e5ddaac6109d5
Edward Kopp <edward@edwardkopp.com>
2026-01-31 21:54:54 -0500

    attempt another fix at fixing methods calling screen commands

--- bedrock_server/_server.py
+++ bedrock_server/_server.py
@@ -40,7 +40,7 @@ class BedrockServer(SystemUtilities):
 
     def start(self) -> None:
         self._download()
-        run(["screen", "-dmS", self._session_name, f"\"./{self.starter_path}\""])
+        run(["screen", "-dmS", self._session_name, f"bash \"{self.starter_path}\""])
 
     def stop(self, force_stop: bool = False) -> None:
         players_online = _BedrockServerStatus("127.0.0.1", self.port_number).status().players.online
@@ -56,7 +56,7 @@ class BedrockServer(SystemUtilities):
         rmtree(self.folder, ignore_errors=True)
 
     def _execute(self, command: str) -> None:
-        run(["screen", "-S", self._session_name, "-p", 0, "-X", "stuff", command + "\n"])
+        run(["screen", "-S", self._session_name, "-p", 0, "-X", "stuff", f"\"{command.replace("\"", "\\\"")}\n\""])
 
     def _download(self, force_download: bool = False) -> None:
         download_and_place(self, force_download)
