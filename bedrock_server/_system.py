55b7e11a3f9b936e7929db83fd458b9fc35210c2
Edward Kopp <edward@edwardkopp.com>
2026-02-05 15:55:44 -0500

    combine _system.py and _update.py, and fix case where server properties will be attempted to be loaded for a server that doesn't exist

--- bedrock_server/_system.py
+++ bedrock_server/_system.py
@@ -3,18 +3,46 @@ from os.path import join, isfile
 from pathlib import Path
 from datetime import datetime
 from shutil import make_archive
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+from subprocess import run
+from fake_useragent import UserAgent
 
 
 class SystemUtilities:
 
+    # File paths and names
     _DIR = join(Path.home(), "BSW")
     _BEDROCK_SERVER_PROGRAM_NAME = "bedrock_server"
     _BEDROCK_SERVER_PROPERTIES_FILE_NAME = "server.properties"
 
+    # URL and headers for update
+    _UPDATE_LINKS_URL = "https://net-secondary.web.minecraft-services.net/api/v1.0/download/links"
+    _USER_AGENT = {
+        "User-Agent": UserAgent(platforms=["desktop"]).random
+    }
+
+    # Server files and directories to normally exclude from updating and replacing
+    _UPDATER_EXCLUDE_FILES = [
+        "allowlist.json",
+        "packetlimitconfig.json",
+        "permissions.json",
+        "profanity_filter.wlist",
+        "server.properties"
+    ]
+    _UPDATER_EXCLUDE_DIRS = [
+        "config"
+    ]
+
+    # Empty dictionary to load server.properties into when ready
+    _SERVER_PROPERTIES: dict[str, str] = {}
+
     def __init__(self, server_name: str) -> None:
         Path(self._DIR).mkdir(parents=True, exist_ok=True)
         self.server_name = server_name.lower()
-        self._SERVER_PROPERTIES: dict[str, str] = {}
+
+    def _load_server_properties(self) -> None:
         with open(join(self.server_subfolder, self._BEDROCK_SERVER_PROPERTIES_FILE_NAME), "r") as file:
             for line in file:
                 line = line.strip()
@@ -65,6 +93,7 @@ class SystemUtilities:
             file.write(url)
 
     def get_server_property(self, key: str) -> str | None:
+        self._load_server_properties()
         return self._SERVER_PROPERTIES.get(key, None)
 
     @property
@@ -137,3 +166,61 @@ class SystemUtilities:
     @staticmethod
     def list_servers() -> list[str]:
         return [server for server in listdir(SystemUtilities._DIR) if SystemUtilities(server).executable_and_properties_exist()]
+
+    @staticmethod
+    def _get_download_url() -> str:
+        try:
+            response = requests.get(SystemUtilities._UPDATE_LINKS_URL, headers=SystemUtilities._USER_AGENT, timeout=10)
+            response.raise_for_status()
+            urls_data: dict = response.json()
+        except requests.Timeout:
+            raise TimeoutError("Request for download links timed out.")
+        except requests.HTTPError:
+            raise ConnectionError("Request for download links unsuccessful.")
+        urls_data: list[dict] = urls_data["result"]["links"]
+        download_url: str | None = None
+        for url_data in urls_data:
+            if url_data["downloadType"] == "serverBedrockLinux":
+                download_url = url_data["downloadUrl"]
+        if download_url is None:
+            raise KeyError("No download URL for Linux server found.")
+        return download_url
+
+    def check_for_update(self) -> bool:
+        return self.last_update_url == self._get_download_url()
+
+    def download_and_place(self, overwrite_all: bool = False) -> None:
+        """
+
+            Used to determine where and to download and how to handle updating if server files already exist.
+        :param overwrite_all: Boolean indicating if download should overwrite existing files.
+        """
+        download_url = self._get_download_url()
+        if not self.executable_and_properties_exist():
+            overwrite_all = True
+        if self.last_update_url == download_url and not overwrite_all:
+            return
+        try:
+            server_zip_response = requests.get(download_url, headers=self._USER_AGENT, timeout=30)
+            server_zip_response.raise_for_status()
+            server_zip_data = server_zip_response.content
+        except requests.Timeout:
+            raise TimeoutError("Request for server download timed out.")
+        except requests.HTTPError:
+            raise ConnectionError("Request for server download unsuccessful.")
+        self.last_update_url = download_url
+        with ZipFile(BytesIO(server_zip_data)) as server_zip:
+            if overwrite_all:
+                members = server_zip.namelist()
+            else:
+                members = [file for file in server_zip.namelist() if file not in self._UPDATER_EXCLUDE_FILES]
+                members = [file for file in members for folder in self._UPDATER_EXCLUDE_DIRS if not file.startswith(folder)]
+            server_zip.extractall(self.server_subfolder, members=members)
+        with open(self.starter_path, "w") as starter_file:
+            starter_file.writelines([
+                "#!/usr/bin/env bash\n",
+                f"cd \"{self.server_subfolder}\"\n",
+                "LD_LIBRARY_PATH=. ./bedrock_server"
+            ])
+        run(["chmod", "+x", self.executable_path], check=True)
+        run(["chmod", "+x", self.starter_path], check=True)
