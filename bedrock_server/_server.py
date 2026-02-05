07d2f31a52a51fa459da97f0163930c5438bd86b
Edward Kopp <edward@edwardkopp.com>
2026-02-05 16:04:00 -0500

    move updater functions from SystemUtilities to BedrockServer

--- bedrock_server/_server.py
+++ bedrock_server/_server.py
@@ -3,12 +3,35 @@ from subprocess import run
 from shutil import rmtree
 from re import sub
 from mcstatus import BedrockServer as _BedrockServerStatus
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+from fake_useragent import UserAgent
 
 
 class BedrockServer(SystemUtilities):
 
+    # Server name length limits
     MIN_NAME_LEN = 4
     MAX_NAME_LEN = 32
+
+    # URL and headers for update
+    _UPDATE_LINKS_URL = "https://net-secondary.web.minecraft-services.net/api/v1.0/download/links"
+    _USER_AGENT = {
+        "User-Agent": UserAgent(platforms=["desktop"]).random
+    }
+
+    # Server files and directories to normally exclude from updating and replacing
+    _UPDATER_EXCLUDE_FILES = [
+        "allowlist.json",
+        "packetlimitconfig.json",
+        "permissions.json",
+        "profanity_filter.wlist",
+        "server.properties"
+    ]
+    _UPDATER_EXCLUDE_DIRS = [
+        "config"
+    ]
 
     def __init__(self, server_name: str) -> None:
         """
@@ -104,10 +127,10 @@ class BedrockServer(SystemUtilities):
     def _execute(self, command: str) -> None:
         run(["screen", "-S", self._session_name, "-p", "0", "-X", "stuff", f"{command}\\n"])
 
-    def _download(self, force_download: bool = False) -> None:
+    def _download(self) -> None:
         if self._check_running():
             raise RuntimeError("Cannot download server while it is running.")
-        self.download_and_update(force_download)
+        self.download_and_update()
 
     def get_player_count(self) -> int:
         return _BedrockServerStatus("127.0.0.1", self.port_number).status().players.online
@@ -116,3 +139,55 @@ class BedrockServer(SystemUtilities):
     def list_online_servers() -> list[str]:
         active_sessions_display = BedrockServer._active_screen_sessions_display()
         return [server for server in SystemUtilities.list_servers() if server in active_sessions_display]
+
+    def check_for_update(self) -> bool:
+        return self.last_update_url == self._get_download_url()
+
+    @staticmethod
+    def _get_download_url() -> str:
+        try:
+            response = requests.get(BedrockServer._UPDATE_LINKS_URL, headers=BedrockServer._USER_AGENT, timeout=10)
+            response.raise_for_status()
+            urls_data: dict = response.json()
+        except requests.Timeout:
+            raise TimeoutError("Request for download links timed out.")
+        except requests.HTTPError:
+            raise ConnectionError("Request for download links unsuccessful.")
+        urls_data: list[dict] = urls_data["result"]["links"]
+        download_url: str | None = None
+        for url_data in urls_data:
+            if url_data["downloadType"] == "serverBedrockLinux":
+                download_url = url_data["downloadUrl"]
+        if download_url is None:
+            raise KeyError("No download URL for Linux server found.")
+        return download_url
+
+    def download_and_update(self) -> None:
+        download_url = self._get_download_url()
+        overwrite_all = False if self.executable_and_properties_exist() else True
+        if self.last_update_url == download_url and not overwrite_all:
+            return
+        try:
+            server_zip_response = requests.get(download_url, headers=self._USER_AGENT, timeout=30)
+            server_zip_response.raise_for_status()
+            server_zip_data = server_zip_response.content
+        except requests.Timeout:
+            raise TimeoutError("Request for server download timed out.")
+        except requests.HTTPError:
+            raise ConnectionError("Request for server download unsuccessful.")
+        self.last_update_url = download_url
+        with ZipFile(BytesIO(server_zip_data)) as server_zip:
+            if overwrite_all:
+                members = server_zip.namelist()
+            else:
+                members = [file for file in server_zip.namelist() if file not in self._UPDATER_EXCLUDE_FILES]
+                members = [file for file in members for folder in self._UPDATER_EXCLUDE_DIRS if not file.startswith(folder)]
+            server_zip.extractall(self.server_subfolder, members=members)
+        with open(self.starter_path, "w") as starter_file:
+            starter_file.writelines([
+                "#!/usr/bin/env bash\n",
+                f"cd \"{self.server_subfolder}\"\n",
+                "LD_LIBRARY_PATH=. ./bedrock_server"
+            ])
+        run(["chmod", "+x", self.executable_path], check=True)
+        run(["chmod", "+x", self.starter_path], check=True)
--- bedrock_server/_system.py
+++ bedrock_server/_system.py
@@ -3,11 +3,6 @@ from os.path import join, isfile
 from pathlib import Path
 from datetime import datetime
 from shutil import make_archive
-import requests
-from zipfile import ZipFile
-from io import BytesIO
-from subprocess import run
-from fake_useragent import UserAgent
 
 
 class SystemUtilities:
@@ -16,24 +11,6 @@ class SystemUtilities:
     _DIR = join(Path.home(), "BSW")
     _BEDROCK_SERVER_PROGRAM_NAME = "bedrock_server"
     _BEDROCK_SERVER_PROPERTIES_FILE_NAME = "server.properties"
-
-    # URL and headers for update
-    _UPDATE_LINKS_URL = "https://net-secondary.web.minecraft-services.net/api/v1.0/download/links"
-    _USER_AGENT = {
-        "User-Agent": UserAgent(platforms=["desktop"]).random
-    }
-
-    # Server files and directories to normally exclude from updating and replacing
-    _UPDATER_EXCLUDE_FILES = [
-        "allowlist.json",
-        "packetlimitconfig.json",
-        "permissions.json",
-        "profanity_filter.wlist",
-        "server.properties"
-    ]
-    _UPDATER_EXCLUDE_DIRS = [
-        "config"
-    ]
 
     # Empty dictionary to load server.properties into when ready
     _SERVER_PROPERTIES: dict[str, str] = {}
@@ -166,61 +143,3 @@ class SystemUtilities:
     @staticmethod
     def list_servers() -> list[str]:
         return [server for server in listdir(SystemUtilities._DIR) if SystemUtilities(server).executable_and_properties_exist()]
-
-    @staticmethod
-    def _get_download_url() -> str:
-        try:
-            response = requests.get(SystemUtilities._UPDATE_LINKS_URL, headers=SystemUtilities._USER_AGENT, timeout=10)
-            response.raise_for_status()
-            urls_data: dict = response.json()
-        except requests.Timeout:
-            raise TimeoutError("Request for download links timed out.")
-        except requests.HTTPError:
-            raise ConnectionError("Request for download links unsuccessful.")
-        urls_data: list[dict] = urls_data["result"]["links"]
-        download_url: str | None = None
-        for url_data in urls_data:
-            if url_data["downloadType"] == "serverBedrockLinux":
-                download_url = url_data["downloadUrl"]
-        if download_url is None:
-            raise KeyError("No download URL for Linux server found.")
-        return download_url
-
-    def check_for_update(self) -> bool:
-        return self.last_update_url == self._get_download_url()
-
-    def download_and_update(self, overwrite_all: bool = False) -> None:
-        """
-
-            Used to determine where and to download and how to handle updating if server files already exist.
-        :param overwrite_all: Boolean indicating if download should overwrite existing files.
-        """
-        download_url = self._get_download_url()
-        if not self.executable_and_properties_exist():
-            overwrite_all = True
-        if self.last_update_url == download_url and not overwrite_all:
-            return
-        try:
-            server_zip_response = requests.get(download_url, headers=self._USER_AGENT, timeout=30)
-            server_zip_response.raise_for_status()
-            server_zip_data = server_zip_response.content
-        except requests.Timeout:
-            raise TimeoutError("Request for server download timed out.")
-        except requests.HTTPError:
-            raise ConnectionError("Request for server download unsuccessful.")
-        self.last_update_url = download_url
-        with ZipFile(BytesIO(server_zip_data)) as server_zip:
-            if overwrite_all:
-                members = server_zip.namelist()
-            else:
-                members = [file for file in server_zip.namelist() if file not in self._UPDATER_EXCLUDE_FILES]
-                members = [file for file in members for folder in self._UPDATER_EXCLUDE_DIRS if not file.startswith(folder)]
-            server_zip.extractall(self.server_subfolder, members=members)
-        with open(self.starter_path, "w") as starter_file:
-            starter_file.writelines([
-                "#!/usr/bin/env bash\n",
-                f"cd \"{self.server_subfolder}\"\n",
-                "LD_LIBRARY_PATH=. ./bedrock_server"
-            ])
-        run(["chmod", "+x", self.executable_path], check=True)
-        run(["chmod", "+x", self.starter_path], check=True)
